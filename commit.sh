cf5626364ac47253c65ca88cb25c790106a912a9
ci-check <yutakakinjyo+bot@gmail.com>
2016-01-09 10:02:36 +0900

    fix equal strings

--- commit.sh
+++ commit.sh
@@ -6,11 +6,12 @@ git config --global user.name "ci-check"
 git add report.txt
 
 re=`git status -s | grep "^[MAD]" | cut -f 3 -d " "`
-if [ $re -eq  "report.txt" ] ;then
+if [ "$re" = "report.txt" ]; then
     git commit -m "report is changed [ci skip]"
+    git push origin `git rev-parse --abbrev-ref HEAD`
 fi
 
-git push origin `git rev-parse --abbrev-ref HEAD`
+
 
 
 
